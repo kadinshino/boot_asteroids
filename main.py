076a994665178cbfbe81cc9cd1e53b36640e1e73
kadin shino <71521025+kadinshino@users.noreply.github.com>
2025-01-22 16:19:29 -0800

    update

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
-def main(): 
+def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")  # Space after colon
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
