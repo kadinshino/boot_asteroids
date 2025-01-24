3c854d12ea40f37dc4b7708b92e5c3d6b663578e
kadin shino <71521025+kadinshino@users.noreply.github.com>
2025-01-24 14:08:09 -0800

    update

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
 
 
 def main():
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")  # Space after colon
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,7 +12,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -22,10 +27,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
