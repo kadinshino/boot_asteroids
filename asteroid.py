de361eaeed5b4d011fab5e687177f5e47dba7ad9
kadin shino <71521025+kadinshino@users.noreply.github.com>
2025-01-25 14:43:30 -0800

    update

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,13 @@
-def __init__(self, x, y, radius):
-    class Asteroid:
-    
-    Asteroid.containers = (asteroids, updatable, drawable)
+import pygame
+from circleshape import CircleShape
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
